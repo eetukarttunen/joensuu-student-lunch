0f87ccf167c0ed417921f7d693eec1f98d9cffcd
Eetu Karttunen <eetu.karttunen@outlook.com>
2023-07-20 19:32:11 +0300

    added frontend styles and data filtering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,56 @@
 // src/App.js
 import React, { useState, useEffect } from 'react';
 import RestaurantBox from './RestaurantBox';
+import "./App.css";
 
 const App = () => {
   const [restaurantData, setRestaurantData] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:3001/menus')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('Received data:', data);
-      if (Array.isArray(data)) {
-        setRestaurantData(data);
-      } else {
-        console.error('Invalid data format received:', data);
-      }
-    })
-    .catch((error) => {
-      console.error('Error fetching data:', error);
-    });
-}, []);
+      .then((response) => response.json())
+      .then((data) => {
+        if (Array.isArray(data)) {
+          setRestaurantData(data);
+        } else {
+          console.error('Invalid data format received:', data);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
-  return (
-    <div className="app">
-      {restaurantData.map((restaurant) => (
-        <RestaurantBox
-          key={restaurant.name}
-          name={restaurant.name}
-          data={restaurant.data}
-          error={restaurant.error}
-        />
-      ))}
+  // Separate boxes with data from empty boxes
+  const boxesWithData = restaurantData.filter((restaurant) => restaurant.data && restaurant.data.MenusForDays.length > 0);
+  const emptyBoxes = restaurantData.filter((restaurant) => !restaurant.data || restaurant.data.MenusForDays.length === 0);
+
+  // Concatenate the arrays with data-first order
+  const sortedRestaurantData = [...boxesWithData, ...emptyBoxes];
+
+  return (<>
+    <div className='App-info'>
+      <h1 className='page-header'>Päivän opiskelijaruoka</h1>
+      <p className="page-info">
+        Kaikki Joensuun alueen yliopisto -ja AMK-ruokaloiden listat samassa näkymässä! &#129382;
+      </p>
     </div>
+    <div className="App">
+
+      <div className="Content">
+
+        {sortedRestaurantData.map((restaurant) => (
+          <RestaurantBox
+            key={restaurant.name}
+            name={restaurant.name}
+            data={restaurant.data}
+            error={restaurant.error}
+          />
+        ))}
+      </div>
+    </div>
+    <footer><a href="https://github.com/eetukarttunen">Copyright © 2023 ietu</a></footer>
+  </>
   );
 };
 
--- frontend/src/RestaurantBox.js
+++ frontend/src/RestaurantBox.js
@@ -1,5 +1,6 @@
 // src/components/RestaurantBox.js
 import React from 'react';
+import './RestaurantBox.css';
 
 const RestaurantBox = ({ name, data, error }) => {
   const currentDate = new Date().toISOString().split('T')[0];
@@ -7,35 +8,38 @@ const RestaurantBox = ({ name, data, error }) => {
     (menuDay) => menuDay.Date.split('T')[0] === currentDate
   );
 
+  
   return (
     <div className="restaurant-box">
-      <h3>{name}</h3>
+      <h2>{name}</h2>
       {error ? (
         <p>Error fetching data: {error}</p>
       ) : filteredMenus.length ? (
-        <ul>
+        <div className="menu-items">
           {filteredMenus.map((menuDay, index) => (
-            <li key={index}>
+            <div key={index} className="menu-day">
               {/*<strong>Date: {menuDay.Date}</strong>*/}
-              <p>Avoinna: {menuDay.LunchTime}</p>
-              <ul>
+              <p>Lunch Time: {menuDay.LunchTime}</p>
+              <ul className='ul-left'>
                 {menuDay.SetMenus.map((menuItem, innerIndex) => (
                   <li key={innerIndex}>
                     <strong>{menuItem.Name}</strong>
                     {/*<p>Price: {menuItem.Price}</p>*/}
                     <ul>
                       {menuItem.Components.map((component, componentIndex) => (
-                        <li key={componentIndex}>{component}</li>
+                        <li key={componentIndex}>
+                          {component}
+                          </li>
                       ))}
                     </ul>
                   </li>
                 ))}
               </ul>
-            </li>
+            </div>
           ))}
-        </ul>
+        </div>
       ) : (
-        <p>Ravintolan {name} lista ei ole saatavilla</p>
+        <p className="not-found">Ravintola suljettu.</p>
       )}
     </div>
   );
