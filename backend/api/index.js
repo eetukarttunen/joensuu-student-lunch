02ad060b1624288584e94cec895b2b863ba2908c
Eetu Karttunen <eetu.karttunen@outlook.com>
2023-07-21 19:58:12 +0300

    deployment stuff

--- backend/api/index.js
+++ backend/api/index.js
@@ -6,8 +6,15 @@ app.use(cors());
 
 app.use(express.json())
 
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
-app.get("/menus", async (req, res) => {
+
+app.get("/api/menus", async (req, res) => {
   try {
     const data = await menuService.getData();
     res.send(data);
