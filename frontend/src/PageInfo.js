a810008a5744c8111cf18c17f4831271885b9a7f
eetukarttunen <eetu.karttunen@outlook.com>
2026-01-05 11:26:27 +0200

    remove info text and update dependencies

--- frontend/src/PageInfo.js
+++ frontend/src/PageInfo.js
@@ -6,8 +6,6 @@ const PageInfo = ({ errorMessage }) => (
     <p>
       Kaikki Joensuun alueen yliopisto- ja AMK-ruokaloiden listat samassa näkymässä! &#129382;
       <br />
-      <br />
-      Klikkaamalla ⭐-ikonia voit siirtää ravintolan listan ensimmäisten joukkoon. Listan piilottaminen onnistuu klikkaamalla ravintolan nimeä.
     </p>
     {errorMessage && (
       <div className="error-message">
